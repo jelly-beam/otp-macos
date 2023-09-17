349bab45268b295d21aaf258cd7c940c6aa555fa
Bryan Paxton <bryan@starbelly.io>
2023-09-16 22:39:57 -0500

    Use `shasum` vs `sha256sum`

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -72,7 +72,7 @@ echo "::endgroup::"
 tar_archive() {
 	file="macos64-${macos_vsn}-OTP-${OTP_VSN}.tar.gz"
 	tar -vzcf "$file" ./*
-    sha256sum  "$file" > "${macos_vsn}-OTP-${OTP_VSN}-sha256.txt"
+    shasum -a 256  "$file" > "${macos_vsn}-OTP-${OTP_VSN}-sha256.txt"
 }
 echo "::group::tar: archive"
 tar_archive
