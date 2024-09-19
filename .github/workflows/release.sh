6d0b38d3a7a6a41d61e0cca8ed69bec48d3be1fa
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2024-09-19 21:03:38 +0100

    Fix the context

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -180,11 +180,10 @@ pick_otp_vsn() {
             local filename_no_ext
             filename_no_ext=$(filename_no_ext_for "${release}")
 
-            if ! grep -qE "^${filename_no_ext}$" .DONT_BUILD; then
+            pushd "${global_INITIAL_DIR}" >/dev/null || exit 1
+            if grep -qE "^${filename_no_ext}$" .DONT_BUILD; then
                 continue
             fi
-
-            pushd "${global_INITIAL_DIR}" >/dev/null || exit 1
             if [[ -f _RELEASES ]]; then
                 local found
                 found=$(grep "${filename_no_ext} " _RELEASES)
